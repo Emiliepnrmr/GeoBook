296cf7720676541464d2ea88c1c968f3bb778d28
gaetan-lavenu <glavenu@MacGaetan.local>
2014-05-05 11:07:13 +0200

    Chargement initial

---
+++ README.md
@@ -0,0 +1,62 @@
+# Livres Cartographiques
+
+Le générateur de "Livres Cartographiques" est une application ArcGIS JavaScript qui permet aux utilisateurs de construire une présentation exposant des contenus interatifs dont des cartes web ArcGIS Online (ou Portal for ArcGIS).
+
+Cette application peut être déployée par toutes les organisations pour publier des documents dynamiques et interactifs pour les décideurs, vos collaborateurs, vos partenaires ou même le grand public. 
+
+Les Livres Cartographiques peuvent contenir des cartes web ArcGIS, des videos, des photos, des textes riches. Vos Livres Cartographiques sont stockés sur la plateforme ArcGIS Online (ou Portal for ArcGIS) de votre organisation. Comme avec n'mporte quelle autre ressource de votre portail, vos livres cartographiques peuvent être privés, partagés avec différents membres de votre organisation ou patagés publiquement. Vos livres cartographiques s'affichent dans un navigateur aussi bien sur des postes Desktop que sur des tablettes.
+
+Le projet "Livre Cartographique" est une version française du projet d'Esri ["Briefing-book"](https://github.com/Esri/briefing-book) traduite et adaptée par Gaëtan LAVENU (Esri France).
+
+
+[Consulter un exemple de Livre Cartographique](http://195.154.180.110/LivreCartographique/default.htm)
+
+[![Image de l'application Livre Cartographique](briefing-book.png "Livre Cartographique")](http://195.154.180.110/LivreCartographique/default.htm)
+
+## Fonctionnalités
+
+* Créer une présentation incluant des cartes web ArcGIS, des vidéos, des photos et des textes riches
+* Partager votre présentation avec différents utilisateurs ou de manière publique 
+
+## Instructions
+
+### Aide générale sur l'utilisation de GitHub (en anglais)
+[New to Github? Get started here.](http://htmlpreview.github.com/?https://github.com/Esri/esri.github.com/blob/master/help/esri-getting-to-know-github.html)
+
+## Démarrer avec ce configurateur
+
+Pour commencer, vous devez installer et paramétrer l'application:
+
+[Configurer l'application (reste à traduire)](http://solutions.arcgis.com/local-government/help/briefing-book/get-started/configure-application/)
+
+[Publier l'application (reste à traduire)](http://solutions.arcgis.com/local-government/help/briefing-book/get-started/publish-application/)
+
+
+## Contributing
+
+Esri France accepte toute proposition de contribution pouvant améliorer cette application
+Please see our [guidelines for contributing](https://github.com/esri/contributing).
+
+## Licensing
+
+Copyright 2013 Esri / Esri France
+
+Cette application est publiée sous licence: Apache License, Version 2.0 (the "License");
+Vous ne devez pas utiliser les fichiers de ce projet en dehors des clauses de cette Licence.
+Les clauses de cette licence sont détaillées ici:
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Vous devez également prendre en compte les limitations suivante:
+
+"Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.""
+
+Une copie des clauses de la licence Apache Version 2.0 se trouve dans ce Repository GitHub (en version originale anglaise)
+[LICENSE.txt](LICENSE.txt).
+
+[](Esri Tags: Local-Government Local Government Briefing Book State-Government State Government ArcGIS Defense and Intelligence Military)
+[](Esri Language: JavaScript)
---
+++ nls/fr/localizedstrings.js
@@ -0,0 +1,112 @@
+/*global define */
+/*jslint browser:true,sloppy:true,nomen:true,unparam:true,plusplus:true */
+/*
+ | Copyright 2013 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ |
+ | Translated by Gaetan LAVENU - Esri France
+ */
+
+define({
+    noPages: "Aucune page trouvée",
+    tocContentsCaption: "Contenus",
+    homeTitle: "Accueil",
+    addPageTitle: "Ajouter une page",
+    alertDialogTitle: "Alerte",
+    confirmDialogTitle: "Confirmation",
+    downloadBookTitle: "Télécharger le GeoBook",
+    refreshBookTitle: "Rafraîchir le GeoBook",
+    editTitle: "Modifier le GeoBook",
+    deleteTitle: "Supprimer la page",
+    descriptionNotAvailable: "Description non disponible",
+    removeBookTitle: "Supprimer le GeoBook",
+    tocTitle: "Contenus",
+    addPageText: "Ajouter",
+    addBookTitle: "Nouveau GeoBook",
+    saveBookShelf: "Enregistrer le GeoBook",
+    copyBookShelf: "Copier le GeoBook",
+    defaultItemId: "nouvelElement",
+    contentsPageTitle: "Contenus",
+    pageRequireMessage: "Un titre est nécessaire pour la page",
+    page: "Page",
+    of: "sur",
+    okButtonText: "OK",
+    saveButtonText: "Enregistrer",
+    cancelButtonText: "Annuler",
+    closeButtonTitle: "Fermer",
+    untitled: "Sans titre",
+    legendTitle: "Légende",
+    fullScreen: "Basculer en plein écran",
+    dndModuleText: "Glisser-Déposer des widgets",
+    selectAnyLayout: "Sélectionner une mise en page ",
+    fieldIsEmpty: "Le champ ne peut pas être vide ",
+    webMapIconTitle: "Carte web",
+    freeFormIconTitle: "HTML",
+    textAreaIconTitle: "Zone de texte",
+    imageIconTitle: "Image",
+    videoIconTitle: "Video",
+    flickrIconTitle: "Flickr ",
+    editMentEditTitle: "Modifier",
+    editMentDeleteTitle: "Supprimer ",
+    confirmModuleDeleting: "Voulez-vous supprimer ce widget ? ",
+    authorName: "Nom de l\'auteur",
+    settingDialogTitle: "Modifier ${modType}",
+    mapbookDefaultTitle: "Le titre du GeoBook",
+    confirmPageDeleting: "Cette page va être supprimée. Cliquer sur \'OK\' pour confirmer.",
+    confirmCopyOfSelectedBook: "Souhaitez-vous copier ce GeoBook ?",
+    confirmDeletingOfSelectedBook: "${bookName} va être supprimé. Cliquer sur \'OK\' pour confirmer la suppression.",
+    confirmAppReloading: "La bibliothèque de GeoBooks va être rechargée et vous perdrez vos modifications.",
+    validateBookOwner: "Vous n\'avez pas la permission de modifier ce GeoBook.",
+    validateOrganizationUser: "Vous n\'êtes pas membre de cette organisation.",
+    copyKeyword: 'Copie de ',
+    signInText: "Se connecter",
+    signOutText: "Se déconnecter",
+    unitInPixel: " (px)",
+    unLockBookTitle: "Verrouiller le GeoBook",
+    lockBookTitle: "Déverrouiller le GeoBook",
+    selectedWebmapText: "Affichage actuel:  <b>${webmapName}</b>. Cliquer sur \'Selectionner une carte\' pour choisir une carte web",
+    selectWebmapBtnText: "Selectionner une carte",
+    selectWebmapDialogTitle: "Selectionner une carte web",
+    searchWebmapPlaceHolder: "Saisir les termes de la recherche",
+    validateUnSavedEdits: "Ce GeoBook contient des modifications non enregistrées. Cliquer sur \'Annuler\' pour rester sur cette page. Cliquer sur \'OK\' pour continuer sans enregistrer.\nAttention : Si vous n\'enregistrez pas votre GeoBook, toutes les modifications réalisées seront perdues.",
+    bookNotSaved: "Ce GeoBook n\'est pas encore enregistré. Cliquer sur l\'icone \'Enregistrer\' pour sauvegarder ce GeoBook.",
+    webmapCountStatus: "${start} - ${end} sur ${total}",
+    noWebmapFound: "Aucune carte trouvée",
+    loadingWebmap: "Chargement...",
+    shareBookTitle: "Partager ce GeoBook",
+    shareBookDialogTitle: "Options de partage",
+    shareToOrgText: "Organisation",
+    shareToEveryoneText: "Tout le monde (public)",
+    protectCopyBookText: "Protéger ce GeoBook contre la copie",
+    copyRestricted: "Impossible de copier, le propriétaire de ce GeoBook l\'a protégé contre la copie.",
+    orientationNotSupported: "L\'application ne supporte pas cette orientation",
+    errorMessages: {
+        webmapSearchFailed: "Une erreur s\'est produite lors de la recherche de la carte",
+        widgetNotLoaded: "Widget non chargé ",
+        updatingItemError: "La mise à jour de l\'élément sélectionné a échouée",
+        contentPageRequired: "Page de contenu est requise ",
+        moduleFieldsEmpty: "Certains champs du module ne sont pas complétés",
+        invalidMapUrl: "URL de la carte web incorrecte ",
+        downloadZipFileFailed: "Le téléchargement a échoué ",
+        fieldInputIsNotValid: "Saisie invalide ",
+        addingItemError: "L\'ajout du  nouvel élément a échoué",
+        deletingItemError: "La suppression de l\'élément a échoué ",
+        updaingItemError: "La mise à jour de l\'élément a échoué",
+        copyItemError: "La copie de l\'élément sélectionné a échoué ",
+        contentQueryError: "Le chargement des contenus de l\'utilisateur a échoué",
+        webmapError: "Une erreur s\'est produite lors du chargement de la carte web ",
+        permissionDenied: "Vous n\avez pas la permission d\accéder à cette ressource.",
+        shareItemError: "Le partage de l\élément sélectionné a échoué"
+    }
+});
